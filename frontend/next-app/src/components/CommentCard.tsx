cd5d82d0c76098dcc4be4dfdaf4c25c870502e0f
Székely Csaba <csabaszekely99@gmail.com>
2025-05-21 13:23:59 +0200

    comment card modified

--- frontend/next-app/src/components/CommentCard.tsx
+++ frontend/next-app/src/components/CommentCard.tsx
@@ -10,7 +10,7 @@ export default function CommentCard({
   date,
 }: CommentCardProps) {
   return (
-    <div className="rounded-xl border border-zinc-200 dark:border-zinc-700 p-4 bg-zinc-50 dark:bg-zinc-900">
+    <div className="rounded-2xl border border-zinc-200 dark:border-zinc-700 p-4 shadow-sm hover:shadow-md transition bg-white dark:bg-zinc-800">
       <div className="flex justify-between items-center mb-1">
         <p className="font-semibold text-zinc-800 dark:text-zinc-100">
           {author}
